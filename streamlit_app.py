28f8c0bc714f135d0b45b2593c42430396e76202
Frishaa <sasharamadhani34@gmail.com>
2025-05-27 11:46:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.image("0974bdaa9f61a3afc1fb004984fb9a37.jpg", width=300)
 
 st.title("Best App")
 st.header("Best App cek Nilai Genap/Ganjil")
-angka = st.number_input("Tulis sebuah angka:"),value=0, step=1
+angka = st.number_input("Tulis sebuah angka:",value=0, step=1)
 
 if (angka % 2) == 0:
   st.write(f"{angka} adalah Bilangan Genap")
