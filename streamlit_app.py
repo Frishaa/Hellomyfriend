47fc273e671f2d5e471e74d383e847d83ec5f389
Frishaa <sasharamadhani34@gmail.com>
2025-05-27 11:25:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈Hello Guys💝💖💣")
+st.title("🎈🧡🧡Hello Guys💝💖💣")
 st.write("😻Thank you for🐯 click this web big love sasha, Xoxo💝")
 
 st.image("pngtree-cute-funny-cat-illustration-png-image_16803570.png", width=300)
