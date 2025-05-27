967994ba171c0e44590f082e0af4fa2d73e08b72
Frishaa <sasharamadhani34@gmail.com>
2025-05-27 11:33:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈🧡🧡Hello Guys💝💖💣")
 st.write("😻Thank you for🐯 click this web big love sasha, Xoxo💝")
 
 st.image("pngtree-cute-funny-cat-illustration-png-image_16803570.png", width=300)
-
+st.image("0974bdaa9f61a3afc1fb004984fb9a37.jpg", width=300)
